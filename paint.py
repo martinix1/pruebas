e2b3159f03418db6779174f5636b70cf5306cfe4
Martin Mtz <martinmtz@MacBook-Pro-8.local>
2025-03-20 15:22:13 -0600

    Correcciones en formas y color morado

--- paint.py
+++ paint.py
@@ -11,17 +11,14 @@ Exercises
 """
 
 from turtle import *
-
 from freegames import vector
 
-
 def line(start, end):
-    """Draw line from start to end."""
+    """Draw a line from start to end."""
     up()
     goto(start.x, start.y)
     down()
     goto(end.x, end.y)
-
 
 def square(start, end):
     """Draw square from start to end."""
@@ -30,12 +27,20 @@ def square(start, end):
     down()
     begin_fill()
 
-    for count in range(4):
+    for _ in range(4):
         forward(end.x - start.x)
         left(90)
 
     end_fill()
 
+def circle(start, end):
+    """Draw circle from start to end."""
+    up()
+    goto(start.x, start.y - (end.x - start.x))  # Moverse al centro inferior
+    down()
+    begin_fill()
+    turtle.circle(end.x - start.x)  # Dibuja el círculo con el radio definido
+    end_fill()
 
 def rectangle(start, end):
     """Draw rectangle from start to end."""
@@ -43,16 +48,17 @@ def rectangle(start, end):
     goto(start.x, start.y)
     down()
     begin_fill()
+    
     width = end.x - start.x
-    height = end.y - start.y
+    height = (end.y - start.y)
+    
     for _ in range(2):
         forward(width)
         left(90)
         forward(height)
         left(90)
+
     end_fill()
-
-
 
 def triangle(start, end):
     """Draw triangle from start to end."""
@@ -60,23 +66,12 @@ def triangle(start, end):
     goto(start.x, start.y)
     down()
     begin_fill()
+    
     for _ in range(3):
         forward(end.x - start.x)
         left(120)
+
     end_fill()
-
-
-def circle(start, end):
-    """Draw circle from start to end."""
-    up()
-    goto(start.x, start.y)
-    down()
-    begin_fill()
-    radius = abs(end.x - start.x)  # Define radio
-    turtle.circle(radius)
-    end_fill()
-
-
 
 def tap(x, y):
     """Store starting point or draw shape."""
@@ -90,23 +85,21 @@ def tap(x, y):
         shape(start, end)
         state['start'] = None
 
-
 def store(key, value):
     """Store value in state at key."""
     state[key] = value
-
 
 state = {'start': None, 'shape': line}
 setup(420, 420, 370, 0)
 onscreenclick(tap)
 listen()
 onkey(undo, 'u')
-onkey(lambda: color('purple'), 'M')
 onkey(lambda: color('black'), 'K')
 onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+onkey(lambda: color('purple'), 'M')  # Agregado color morado
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
